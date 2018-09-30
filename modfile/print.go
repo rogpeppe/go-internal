b3b22305804789c983f38df9973ced94b79a0466
Roger Peppe <rogpeppe@gmail.com>
2018-09-30 20:04:26 +0100

    modinternal/modfile: add package comment

--- modfile/print.go
+++ modfile/print.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Module file printer.
-
+// Package modfile implements parsing and formatting for
+// go.mod files.
 package modfile
 
 import (
