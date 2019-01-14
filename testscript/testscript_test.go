fdae98f528c477ff94b77b3cbbfab25f6303c3b2
Roger Peppe <rogpeppe@gmail.com>
2019-01-14 12:32:03 +0000

    testscript: provide access to background commands
    This adds a method to TestScript to provide access to the
    commands that have been started in the background.
    This enables use cases like testing signal handling in
    commands.
    
    We also move the OS and architecture conditions to
    the base testscript package so that scripts can get
    access to OS conditions without importing gotooltest.

--- testscript/testscript_test.go
+++ testscript/testscript_test.go
@@ -8,9 +8,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func printArgs() int {
@@ -23,10 +25,26 @@ func exitWithStatus() int {
 	return n
 }
 
+func signalCatcher() int {
+	// Note: won't work under Windows.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	// Create a file so that the test can know that
+	// we will catch the signal.
+	if err := ioutil.WriteFile("catchsignal", nil, 0666); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	<-c
+	fmt.Println("caught interrupt")
+	return 0
+}
+
 func TestMain(m *testing.M) {
 	os.Exit(RunMain(m, map[string]func() int{
-		"printargs": printArgs,
-		"status":    exitWithStatus,
+		"printargs":     printArgs,
+		"status":        exitWithStatus,
+		"signalcatcher": signalCatcher,
 	}))
 }
 
@@ -48,13 +66,15 @@ func TestCRLFInput(t *testing.T) {
 	})
 }
 
-func TestSimple(t *testing.T) {
+func TestScripts(t *testing.T) {
 	// TODO set temp directory.
 	Run(t, Params{
 		Dir: "scripts",
 		Cmds: map[string]func(ts *TestScript, neg bool, args []string){
 			"setSpecialVal":    setSpecialVal,
 			"ensureSpecialVal": ensureSpecialVal,
+			"interrupt":        interrupt,
+			"waitfile":         waitFile,
 		},
 	})
 	// TODO check that the temp directory has been removed.
@@ -69,4 +89,41 @@ func ensureSpecialVal(ts *TestScript, neg bool, args []string) {
 	if got := ts.Getenv("SPECIALVAL"); got != want {
 		ts.Fatalf("expected SPECIALVAL to be %q; got %q", want, got)
 	}
+}
+
+// interrupt interrupts the current background command.
+// Note that this will not work under Windows.
+func interrupt(ts *TestScript, neg bool, args []string) {
+	if neg {
+		ts.Fatalf("interrupt does not support neg")
+	}
+	if len(args) > 0 {
+		ts.Fatalf("unexpected args found")
+	}
+	bg := ts.BackgroundCmds()
+	if got, want := len(bg), 1; got != want {
+		ts.Fatalf("unexpected background cmd count; got %d want %d", got, want)
+	}
+	bg[0].Process.Signal(os.Interrupt)
+}
+
+func waitFile(ts *TestScript, neg bool, args []string) {
+	if neg {
+		ts.Fatalf("waitfile does not support neg")
+	}
+	if len(args) != 1 {
+		ts.Fatalf("usage: waitfile file")
+	}
+	path := ts.MkAbs(args[0])
+	for i := 0; i < 100; i++ {
+		_, err := os.Stat(path)
+		if err == nil {
+			return
+		}
+		if !os.IsNotExist(err) {
+			ts.Fatalf("unexpected stat error: %v", err)
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	ts.Fatalf("timed out waiting for %q to be created", path)
 }
