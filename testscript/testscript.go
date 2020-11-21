815d671f4ff9c677548e84cc1c896af2eb1704f5
Fazlul Shahriar <fshahriar@gmail.com>
2020-11-21 12:17:35 -0500

    testscript: set $path on Plan 9
    This is a port of this CL from upstream:
    https://go-review.googlesource.com/126608

--- testscript/testscript.go
+++ testscript/testscript.go
@@ -305,6 +305,9 @@ func (ts *TestScript) setup() string {
 		Cd:      ts.workdir,
 		ts:      ts,
 	}
+	if runtime.GOOS == "plan9" {
+		env.Vars = append(env.Vars, "path="+os.Getenv("path"))
+	}
 	// Must preserve SYSTEMROOT on Windows: https://github.com/golang/go/issues/25513 et al
 	if runtime.GOOS == "windows" {
 		env.Vars = append(env.Vars,
